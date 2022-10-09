6cd3b1945955d43b37a47ef6fbdc763349e53587
aeconick <114342845+aeconick@users.noreply.github.com>
2022-10-09 13:15:53 +0300

    DOM events (unfinished)
    from Mac

--- ADVANCED/domManipulationAndEventsLab/05. Highlight-Active/app.js
+++ ADVANCED/domManipulationAndEventsLab/05. Highlight-Active/app.js
@@ -1,3 +1,13 @@
 function focused() {
-    console.log('TODO:...');
+    let textElements = document.querySelectorAll('div div input');
+    let textArr = Array.from(textElements);
+    for (const el of textArr) {
+        el.addEventListener('focus', function () {
+            for (const a of textArr) {
+                console.log('slay');
+                a.parentElement.classList.remove('focused');
+            }
+            el.parentElement.className = 'focused';
+        });
+    };
 }
