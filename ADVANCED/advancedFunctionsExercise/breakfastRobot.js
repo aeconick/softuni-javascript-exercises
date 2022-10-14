7bfe67626d62c3630e31762a66f2ae00bf32316c
aeconick <114342845+aeconick@users.noreply.github.com>
2022-10-14 20:14:59 +0300

    kill me x2

--- ADVANCED/advancedFunctionsExercise/breakfastRobot.js
+++ ADVANCED/advancedFunctionsExercise/breakfastRobot.js
@@ -1,1 +1,73 @@
-//TODO
+function solution() {
+    let result = {
+        protein: 0,
+        carbohydrate: 0,
+        flavour: 0,
+        fat: 0,
+        apple() {
+            this.carbohydrate -= 1;
+            this.flavour -= 2;
+        },
+        lemonade() {
+            this.carbohydrate -= 10;
+            this.flavour -= 20;
+        },
+        burger() {
+            this.carbohydrate -= 5;
+            this.fat -= 7
+            this.flavour -= 3;
+        },
+        eggs() {
+            this.protein -= 5
+            this.fat -= 1;
+            this.flavour -= 1;
+        },
+        turkey() {
+            this.protein -= 10;
+            this.carbohydrate -= 10;
+            this.fat -= 10;
+            this.flavour -= 10;
+        },
+        report() {
+ 
+            return `protein=${this.protein} carbohydrate=${this.carbohydrate} fat=${this.fat} flavour=${this.flavour}`;
+        }
+ 
+ 
+    } //the closure is the result
+ 
+    return function make(input) {
+ 
+        if (input.includes('restock')) {
+            let [_, microelement, quantity] = input.split(' ');
+            quantity = Number(quantity)
+            result[microelement] += quantity;
+            return 'Success';
+        } else if (input.includes('prepare')) {
+            let [_, recipe, quantity] = input.split(' ');
+            quantity = Number(quantity)
+            for (let i = 0; i < quantity; i++) {
+ 
+                result[recipe]();
+                let entries = Object.entries(result);
+                for (let [key, value] of entries) {
+                        if (value < 0){
+                            return `Error: not enough ${key} in stock`
+                        };
+                }
+            }
+            return 'Success';
+ 
+        } else if (input.includes('report')) {
+            let res = result['report']();
+            return res;
+        }
+    }
+}
+ 
+ 
+ 
+let manager = solution();
+console.log(manager("restock flavour 50")); // Success 
+console.log(manager("prepare lemonade 4")); // Error: not enough carbohydrate in stock 
+console.log(manager(('report')));
--- ADVANCED/advancedFunctionsLab/listProcessor.js
+++ ADVANCED/advancedFunctionsLab/listProcessor.js
@@ -1,7 +1,18 @@
-function solve(input){
+function solve(input) {
+    let processor = (function () {
+        let result = [];
+        return {
+            add: (item) => result.push(item),
+            remove: (item) => result = result.filter(x => x !== item),
+            print: () => console.log(result.join(",")),
+        }
+    })();
 
-    //TODO
-
+    for (let line of input) {
+        let [command, text] = line.split(" ");
+        processor[command](text);
+    }
 }
 
-solve(['add hello', 'add again', 'remove hello', 'add again', 'print']);
+solve(['add hello', 'add again', 'remove hello', 'add again', 'print'])
+solve(['add pesho', 'add george', 'add peter', 'remove peter', 'print'])
